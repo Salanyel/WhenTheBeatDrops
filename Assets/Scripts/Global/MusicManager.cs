f19590f032726d4d99397a165a12213828bccd52
Milan "Edrevan" Irigoyen <edrevan@gmail.com>
2015-11-07 16:39:17 +0100

    Added AmarreTestScene Created MusicManager, added corresponding tags

---
+++ Assets/Scripts/Global/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+    //References
+    AudioSource m_source;
+
+    public AudioClip m_baroque_intro;
+    public AudioClip m_baroque_drop1;
+    public AudioClip m_retroish_intro;
+    public AudioClip m_retroish_drop1;
+
+    // Managing the current audio playing
+    bool intro = false;
+    bool looping = false;
+    bool outro = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_source = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
+
--- Assets/Scripts/Global/Tags.cs
+++ Assets/Scripts/Global/Tags.cs
@@ -4,4 +4,5 @@ using System.Collections;
 public class Tags : MonoBehaviour {
 
 	public static string m_mainCamera = "MainCamera";
+    public static string m_musicManager = "MusicManager";
 }
